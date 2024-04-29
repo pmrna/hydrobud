f8dfef54c4596c8a55b465e4614f2a3292d69bd1
Czean Jacob Arroyo <96815505+pmrna@users.noreply.github.com>
2024-04-30 02:16:26 +0800

    added back deleted files
    added back deleted files

---
+++ lib/widget/list_view_pages/IrrigationPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IrrigationPage extends StatelessWidget {
+  const IrrigationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
