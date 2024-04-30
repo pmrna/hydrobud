0f36e7d36856ddc8ae9e41ee8d7831022749dec6
itsamejuls <143085759+itsamejuls@users.noreply.github.com>
2024-04-30 22:08:01 +0800

    Update List View
    Added some contents in the list view pages

---
+++ lib/widget/list_view_pages/analytics_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AnalyticsPage extends StatelessWidget {
+  const AnalyticsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text('Analytics'),
+      ),
+
+      // rest of codes here
+    );
+  }
+}
